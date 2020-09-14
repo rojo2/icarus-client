8880352551d6ebebfac277101b5dfcaeb4d5f770
AzazelN28 <asakon28@gmail.com>
2020-09-14 17:09:08 +0200

    Fix route plugin

--- plugins/route.js
+++ plugins/route.js
@@ -1,18 +1,20 @@
+const fluxes = ['solar-wind', 'particle', 'electron', 'xray']
+const filters = '12345'.split('')
+const shows = ['forecast', 'alerts']
+
 export default ({ app }) => {
   app.router.beforeEach((to, from, next) => {
     if (
       to.path === '/weather' &&
       (!to.query.flux ||
         !to.query.filter ||
-        !['solar-wind', 'particle', 'electron', 'xray'].includes(
-          to.query.flux
-        ) ||
-        !'1234'.split('').includes(to.query.filter))
+        !fluxes.includes(to.query.flux) ||
+        !filters.includes(to.query.filter))
     ) {
       return next('/weather?flux=solar-wind&filter=1')
     } else if (
       to.path === '/forecast' &&
-      (!to.query.show || !['forecast', 'alerts'].includes(to.query.show))
+      (!to.query.show || !shows.includes(to.query.show))
     ) {
       return next('/forecast?show=forecast')
     }
